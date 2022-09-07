cb8133163cbf8000a90afa36d97af24b13328276
0xvv <public_double_v@protonmail.com>
2022-09-08 01:36:47 +0200

    solved delegation

--- test/Delegation.t.sol
+++ test/Delegation.t.sol
@@ -14,6 +14,5 @@ contract DelagationTest is Test {
 
         DelegationSolver solver = new DelegationSolver();
         solver.solve(0x6FE3b830EA43425ABfb44b059084a7d4A78803Ff);
-        console.log(abi.encodeWithSignature("pwn()"));
     }
 }
